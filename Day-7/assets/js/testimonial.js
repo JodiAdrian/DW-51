75fba0c054cbbde12be3438a9b9b6cc19f1d095c
JODI ADRIAN <18102127@ittelkom-pwt.ac.id>
2023-11-15 15:02:38 +0700

    Day-7 OOP Concept & Testimonials Page

---
+++ Day-7/assets/js/testimonial.js
@@ -0,0 +1,43 @@
+const dataTestiomonial = [
+    {
+      name: "Denada",
+      comment: "Lain kali jangan di ulang ya",
+      rating: 2,
+      image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      name: "Clarence Darrow",
+      comment: "You can only protect your liberties in this world by protecting the other man's freedom. You can only be free if I am free.",
+      rating: 4,
+      image: "https://images.unsplash.com/photo-1460194436988-671f763436b7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      name: "Mahatma Gandhi",
+      comment: "Learn as if you will live forever, live like you will die tomorrow.",
+      rating: 5,
+      image: "https://images.unsplash.com/photo-1630419320857-7087d18ade31?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+      name: "Herman Melville",
+      comment: "btw its okay It is better to fail in originality than to succeed in imitation.",
+      rating: 5,
+      image: "https://images.unsplash.com/photo-1516122402193-39636de115ff?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+  ]
+  
+  function showTestimonial() {
+    let testimonialForHtml = ""
+  
+    dataTestiomonial.forEach(item => {
+      testimonialForHtml += `
+        <div class="testimonial">
+          <img src=${item.image} class="profile-testimonial" />
+          <p class="quote">${item.comment}</p>
+          <p class="author">- ${item.name}</p>
+        </div>
+      `
+    })
+  
+    document.getElementById("testimonials").innerHTML = testimonialForHtml
+  }
+  showTestimonial()
